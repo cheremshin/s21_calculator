94d942607c29d4e4eb66d8a13025515e3c40c52a
Skyte Kelsi <skytekel@si-d9.nsk.21-school.ru>
2023-04-06 21:21:44 +0700

    refactor: Rename stack_t and generic_type_t to stack and generic_type

--- src/Models/utils/stack.c
+++ src/Models/utils/stack.c
@@ -1,35 +1,35 @@
 #include "stack.h"
 
 
-char get_char(generic_data_t data) {
+char get_char(generic_data data) {
     return data.value.c;
 }
 
-double get_double(generic_data_t data) {
+double get_double(generic_data data) {
     return data.value.d;
 }
 
-void set_char(generic_data_t *data, char value) {
+void set_char(generic_data *data, char value) {
     data->value.c = value;
     data->type_id = CHAR_TYPE_ID;
 }
 
-void set_double(generic_data_t *data, double value) {
+void set_double(generic_data *data, double value) {
     data->value.d = value;
     data->type_id = DOUBLE_TYPE_ID;
 }
 
-stack_t *init_stack(int type_id) {
-    stack_t *out = NULL;
+stack *init_stack(int type_id) {
+    stack *out = NULL;
 
-    out = malloc(sizeof(stack_t));
+    out = malloc(sizeof(stack));
 
     if (out == NULL) {
         perror("init_stack");
     } else {
         out->size = INIT_SIZE;
         out->top = 0;
-        out->data = malloc(out->size * sizeof(generic_data_t));
+        out->data = malloc(out->size * sizeof(generic_data));
 
         if (out->data == NULL) {
             free(out);
@@ -42,15 +42,15 @@ stack_t *init_stack(int type_id) {
     return out;
 }
 
-void delete_stack(stack_t **stack) {
+void delete_stack(stack **stack) {
     free((*stack)->data);
     free(*stack);
     *stack = NULL;
 }
 
-void resize_stack(stack_t *stack) {
-    generic_data_t *tmp = realloc(stack->data,
-                                  stack->size * MULTIPLIER * sizeof(generic_data_t));
+void resize_stack(stack *stack) {
+    generic_data *tmp = realloc(stack->data,
+                                  stack->size * MULTIPLIER * sizeof(generic_data));
     
     if (tmp == NULL) {
         perror("resize_stack");
@@ -60,7 +60,7 @@ void resize_stack(stack_t *stack) {
     }
 }
 
-void push(stack_t *stack, generic_data_t value) {
+void push(stack *stack, generic_data value) {
     if (stack->top == stack->size) {
         resize_stack(stack);
     }
@@ -70,8 +70,8 @@ void push(stack_t *stack, generic_data_t value) {
     }
 }
 
-generic_data_t *pop(stack_t *stack) {
-    generic_data_t *out = NULL;
+generic_data *pop(stack *stack) {
+    generic_data *out = NULL;
 
     if (stack->top > 0) {
         stack->top--;
@@ -81,8 +81,8 @@ generic_data_t *pop(stack_t *stack) {
     return out;
 }
 
-generic_data_t *peek(stack_t *stack) {
-    generic_data_t *out = NULL;
+generic_data *peek(stack *stack) {
+    generic_data *out = NULL;
 
     if (stack->top > 0) {
         out = &stack->data[stack->top - 1];
