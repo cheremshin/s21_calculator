9753a98c30498bcfa14d15f5bd2e16ea74ba0744
Skyte Kelsi <skytekel@si-d9.nsk.21-school.ru>
2023-04-06 17:30:11 +0700

    feat: Change checks order

--- src/Models/translator/checks.c
+++ src/Models/translator/checks.c
@@ -7,14 +7,14 @@ int is_digit(char symbol) {
 int is_prefix_function(char symbol) {
     int result = 0;
 
+    if (symbol == '~' || symbol == '$') {
+        result = 1;
+    }
+
     for (int i = 0; i < SHORTCUTS_COUNT && result == 0; i++) {
         if (symbol == operation_shortcuts[i].shortcut) {
             result = 1;
         }
-    }
-
-    if (symbol == '~' || symbol == '$') {
-        result = 1;
     }
 
     return result;
