6e2ce5a38c7419698ef0ad9b613373289b846ac1
cheremshin <sergey.cheremshin@gmail.com>
2023-05-20 16:07:32 +0700

    feat: Add x variable support and do some refactor by using new checks

--- src/Models/translator/translator.c
+++ src/Models/translator/translator.c
@@ -2,21 +2,21 @@
 
 
 stack_c *translate_to_polish(char *string) {
-    char operation;
     stack_c *out = init_stack();
     stack_c *operations = init_stack();
 
     for (int i = 0; string[i] != '\0';) {
         if (is_digit(string[i])) {
             process_number(string, &i, out);
-        } else if (is_prefix_function(string[i]) || string[i] == '(') {
+        } else if (is_x(string[i])) {
+            process_x(string, &i, out);
+        } else if (is_prefix_function(string[i]) || is_open_bracket(string[i])) {
             process_prefix_function(string, &i, operations);
-        } else if (string[i] == ')') {
+        } else if (is_close_bracket(string[i])) {
             process_close_bracket(string, &i, operations, out);
         } else {
             process_binary_operation(string, &i, operations, out);
         }
-        set_space(out);
     }
 
     transfer_data(operations, out);
