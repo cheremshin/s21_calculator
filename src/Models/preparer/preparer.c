d10141fff9195cf2da0b839f1ae37616fa4d44a1
cheremshin <sergey.cheremshin@gmail.com>
2023-05-22 05:48:37 +0700

    feat: Write prepare_to_translate lifetime

--- src/Models/preparer/preparer.c
+++ src/Models/preparer/preparer.c
@@ -2,5 +2,36 @@
 
 
 char *prepare_to_translate(char *string) {
-    return NULL;
+    int status = 0;
+    char *out = NULL;
+
+    // Validation
+    status = check_brackets(string);
+    if (status) check_numbers(string);
+    if (status) check_symbols(string);
+
+    // Preparing
+    if (status) {
+        out = (char *)malloc(EXPRESSION_LENGTH * sizeof(char));
+        strcpy(out, string);
+        remove_spaces(out);
+        convert_symbols(out);
+    }
+
+    return out;
+}
+
+void remove_spaces(char *string) {
+    char *copy = (char *)malloc(EXPRESSION_LENGTH * sizeof(char));
+    strcpy(string, copy);
+
+    int j = 0;
+    for (int i = 0; copy[i] != '\0'; i++) {
+        if (copy[i] != ' ') string[j++] = copy[i];
+    }
+    string[j] = '\0';
+}
+
+void convert_symbols(char *string) {
+    
 }
