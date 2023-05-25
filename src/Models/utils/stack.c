0275bbc8f0614fc71b4eaa1807f248d281c77246
cheremshin <sergey.cheremshin@gmail.com>
2023-05-25 23:52:48 +0700

    refactor: Rename stack structure

--- src/Models/utils/stack.c
+++ src/Models/utils/stack.c
@@ -1,8 +1,8 @@
 #include "stack.h"
 
 
-stack *init_stack(int type) {
-    stack *stack = malloc(sizeof(stack));
+stack_t *init_stack(int type) {
+    stack_t *stack = malloc(sizeof(stack_t));
 
     stack->size = INIT_SIZE;
     stack->top = 0;
@@ -12,13 +12,13 @@ stack *init_stack(int type) {
     return stack;
 }
 
-void delete_stack(stack **stack) {
+void delete_stack(stack_t **stack) {
     free((*stack)->elements);
     free(*stack);
     *stack = NULL;
 }
 
-void resize_stack(stack *stack) {
+void resize_stack(stack_t *stack) {
     stack_element *tmp = realloc(stack->elements,
                         stack->size * MULTIPLIER * sizeof(stack_element));
     
@@ -28,7 +28,7 @@ void resize_stack(stack *stack) {
     }
 }
 
-void push(stack *stack, void *value) {
+void push(stack_t *stack, void *value) {
     if (stack->top == stack->size) {
         resize_stack(stack);
     }
@@ -43,7 +43,7 @@ void push(stack *stack, void *value) {
     }
 }
 
-void pop(stack *stack, void *p) {
+void pop(stack_t *stack, void *p) {
     if (stack->top > 0) {
         stack->top--;
 
@@ -59,7 +59,7 @@ void pop(stack *stack, void *p) {
     }
 }
 
-void peek(stack *stack, void *p) {
+void peek(stack_t *stack, void *p) {
     if (stack->top > 0) {
         switch (stack->type)
         {
@@ -73,7 +73,7 @@ void peek(stack *stack, void *p) {
     }
 }
 
-void transfer_data(stack *source, stack *destination) {
+void transfer_data(stack_t *source, stack_t *destination) {
     void *value = malloc(sizeof(double));
 
     while (source->top > 0) {
@@ -82,7 +82,7 @@ void transfer_data(stack *source, stack *destination) {
     }
 }
 
-void convert_to_string(stack *source, char *destination) {
+void convert_to_string(stack_t *source, char *destination) {
     for (int i = 0; i < source->top; i++) {
         destination[i] = source->elements[i].data.value_c;
     }
--- src/Models/utils/stack.h
+++ src/Models/utils/stack.h
@@ -29,12 +29,12 @@ typedef struct stack_element {
  * @brief Struct represents stack
  * 
  */
-typedef struct stack {
+typedef struct stack_t {
     int type;
     stack_element *elements;
     size_t size;
     size_t top;
-} stack;
+} stack_t;
 
 /*************************
  *    Stack functions    *
@@ -46,21 +46,21 @@ typedef struct stack {
  * @param type Type of stored data
  * @return Pointer to stack structure
  */
-stack *init_stack(int type);
+stack_t *init_stack(int type);
 
 /**
  * @brief Functuon that clears stack allocated memory and sets NULL in it
  * 
  * @param stack Stack to be cleared
  */
-void delete_stack(stack **stack);
+void delete_stack(stack_t **stack);
 
 /**
  * @brief Util function that doubles the allocated stack data memory
  * 
  * @param stack Stack to be doubled
  */
-void resize_stack(stack *stack);
+void resize_stack(stack_t *stack);
 
 /**
  * @brief Function that pushes some value to stack
@@ -68,7 +68,7 @@ void resize_stack(stack *stack);
  * @param stack Stack for pushing data into it
  * @param value Value to be pushed
  */
-void push(stack *stack, void *value);
+void push(stack_t *stack, void *value);
 
 /**
  * @brief Function that removes top of stack and sets to p
@@ -76,7 +76,7 @@ void push(stack *stack, void *value);
  * @param stack Stack for removing data from it
  * @param p void* Pointer to a memory to store popped data
  */
-void pop(stack *stack, void *p);
+void pop(stack_t *stack, void *p);
 
 /**
  * @brief Funtction that sets top of stack to p without removing
@@ -84,7 +84,7 @@ void pop(stack *stack, void *p);
  * @param stack Stack to get its top
  * @param p void* Pointer to a memory to store top of stack
  */
-void peek(stack *stack, void *p);
+void peek(stack_t *stack, void *p);
 
 /**
  * @brief Function that pushes all data from source stack to destination with removing from first one
@@ -92,7 +92,7 @@ void peek(stack *stack, void *p);
  * @param source Stack to popping data from
  * @param destination Stack for pushing data to
  */
-void transfer_data(stack *source, stack *destination);
+void transfer_data(stack_t *source, stack_t *destination);
 
 /**
  * @brief Function that moves all stack's elements to string
@@ -100,6 +100,6 @@ void transfer_data(stack *source, stack *destination);
  * @param source Stack to get elements from
  * @param destination String for moving elements to
  */
-void convert_to_string(stack *source, char *destination);
+void convert_to_string(stack_t *source, char *destination);
 
 #endif //  SRC_MODELS_UTILS_STACK_H_
