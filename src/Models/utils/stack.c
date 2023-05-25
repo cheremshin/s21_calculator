e6b4568f558d6dc4c8e7f8ea922f210ff30004dd
cheremshin <sergey.cheremshin@gmail.com>
2023-05-25 23:23:09 +0700

    feat: Add support of multiple types

--- src/Models/utils/stack.c
+++ src/Models/utils/stack.c
@@ -1,63 +1,89 @@
 #include "stack.h"
 
 
-stack_c *init_stack() {
-    stack_c *stack = malloc(sizeof(stack_c));
+stack *init_stack(int type) {
+    stack *stack = malloc(sizeof(stack));
 
     stack->size = INIT_SIZE;
     stack->top = 0;
-    stack->data = malloc(stack->size * sizeof(char));
+    stack->type = type;
+    stack->elements = (stack_element *)malloc(stack->size * sizeof(stack_element));
 
     return stack;
 }
 
-void delete_stack(stack_c **stack) {
-    free((*stack)->data);
+void delete_stack(stack **stack) {
+    free((*stack)->elements);
     free(*stack);
     *stack = NULL;
 }
 
-void resize_stack(stack_c *stack) {
-    char *tmp = realloc(stack->data,
-                        stack->size * MULTIPLIER * sizeof(char));
+void resize_stack(stack *stack) {
+    stack_element *tmp = realloc(stack->elements,
+                        stack->size * MULTIPLIER * sizeof(stack_element));
     
     if (tmp != NULL) {
-        stack->data = tmp;
+        stack->elements = tmp;
         stack->size *= MULTIPLIER;
     }
 }
 
-void push(stack_c *stack, char value) {
+void push(stack *stack, void *value) {
     if (stack->top == stack->size) {
         resize_stack(stack);
     }
 
-    stack->data[stack->top++] = value;
+    switch (stack->type)
+    {
+    case CHAR_TYPE:
+        stack->elements[stack->top++].data.value_c = *((char *) value);
+        break;
+    case DOUBLE_TYPE:
+        stack->elements[stack->top++].data.value_d = *((double *) value);
+    }
 }
 
-char pop(stack_c *stack) {
-    char out = 0;
-
+void pop(stack *stack, void *p) {
     if (stack->top > 0) {
         stack->top--;
-        out = stack->data[stack->top];
-    }
 
-    return out;
+        switch (stack->type)
+        {
+        case CHAR_TYPE:
+            *((char *) p) = stack->elements[stack->top].data.value_c;
+            break;
+        case DOUBLE_TYPE:
+            *((double *) p) = stack->elements[stack->top].data.value_d;
+            break;
+        }
+    }
 }
 
-char peek(stack_c *stack) {
-    char out = 0;
-
+void peek(stack *stack, void *p) {
     if (stack->top > 0) {
-        out = stack->data[stack->top - 1];
+        switch (stack->type)
+        {
+        case CHAR_TYPE:
+            *((char *) p) = stack->elements[stack->top - 1].data.value_c;
+            break;
+        case DOUBLE_TYPE:
+            *((double *) p) = stack->elements[stack->top - 1].data.value_d;
+            break;
+        }
     }
-
-    return out;
 }
 
-void transfer_data(stack_c *source, stack_c *destination) {
+void transfer_data(stack *source, stack *destination) {
+    void *value = malloc(sizeof(double));
+
     while (source->top > 0) {
-        push(destination, pop(source));
+        pop(source, value);
+        push(destination, value);
+    }
+}
+
+void convert_to_string(stack *source, char *destination) {
+    for (int i = 0; i < source->top; i++) {
+        destination[i] = source->elements[i].data.value_c;
     }
 }
--- src/Models/utils/stack.h
+++ src/Models/utils/stack.h
@@ -9,22 +9,32 @@
  *        Defines        *
  ************************/
 
-#define INIT_SIZE        256
-#define MULTIPLIER       2
+#define INIT_SIZE   256
+#define MULTIPLIER  2
+#define CHAR_TYPE   0b00000001
+#define DOUBLE_TYPE 0b00000010
 
 /*************************
  *      Structures       *
  ************************/
 
+typedef struct stack_element {
+    union {
+        char value_c;
+        double value_d;
+    } data;
+} stack_element;
+
 /**
  * @brief Struct represents stack
  * 
  */
-typedef struct stack_c {
-    char *data;
+typedef struct stack {
+    int type;
+    stack_element *elements;
     size_t size;
     size_t top;
-} stack_c;
+} stack;
 
 /*************************
  *    Stack functions    *
@@ -33,47 +43,48 @@ typedef struct stack_c {
 /**
  * @brief Function that inits stack
  * 
+ * @param type Type of stored data
  * @return Pointer to stack structure
  */
-stack_c *init_stack();
+stack *init_stack(int type);
 
 /**
  * @brief Functuon that clears stack allocated memory and sets NULL in it
  * 
  * @param stack Stack to be cleared
  */
-void delete_stack(stack_c **stack);
+void delete_stack(stack **stack);
 
 /**
  * @brief Util function that doubles the allocated stack data memory
  * 
  * @param stack Stack to be doubled
  */
-void resize_stack(stack_c *stack);
+void resize_stack(stack *stack);
 
 /**
  * @brief Function that pushes some value to stack
  * 
  * @param stack Stack for pushing data into it
- * @param value Char to be pushed
+ * @param value Value to be pushed
  */
-void push(stack_c *stack, char value);
+void push(stack *stack, void *value);
 
 /**
- * @brief Function that removes top of stack and returns it back
+ * @brief Function that removes top of stack and sets to p
  * 
  * @param stack Stack for removing data from it
- * @return Char data removed from top of stack
+ * @param p void* Pointer to a memory to store popped data
  */
-char pop(stack_c *stack);
+void pop(stack *stack, void *p);
 
 /**
- * @brief Funtction that returns top of stack without removing
+ * @brief Funtction that sets top of stack to p without removing
  * 
  * @param stack Stack to get its top
- * @return Char from top of stack
+ * @param p void* Pointer to a memory to store top of stack
  */
-char peek(stack_c *stack);
+void peek(stack *stack, void *p);
 
 /**
  * @brief Function that pushes all data from source stack to destination with removing from first one
@@ -81,6 +92,14 @@ char peek(stack_c *stack);
  * @param source Stack to popping data from
  * @param destination Stack for pushing data to
  */
-void transfer_data(stack_c *source, stack_c *destination);
+void transfer_data(stack *source, stack *destination);
+
+/**
+ * @brief Function that moves all stack's elements to string
+ * 
+ * @param source Stack to get elements from
+ * @param destination String for moving elements to
+ */
+void convert_to_string(stack *source, char *destination);
 
 #endif //  SRC_MODELS_UTILS_STACK_H_
