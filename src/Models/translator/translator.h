5b1c2f7c297731b59753511312fe7808cb00e5fc
cheremshin <sergey.cheremshin@gmail.com>
2023-05-25 23:56:36 +0700

    refactor: Rename stack type in function definitions

--- src/Models/translator/translator.h
+++ src/Models/translator/translator.h
@@ -22,23 +22,23 @@ extern operation_priority operations_priority[OPERATIONS_COUNT];
  * in infix notation to polish notation
  * 
  * @param string String to be translated
- * @return stack_t Stack consists of char generic data
+ * @return char* Translated string (should be freed)
  */
-stack_c *translate_to_polish(char *string);
+char *translate_to_polish(char *string);
 
 /**************************
  *   Internal functions   *
   ************************/
 
-void process_number(char *string, int *i, stack_c *out);
-void process_x(char *string, int *i, stack_c *out);
-void process_prefix_function(char *string, int *i, stack_c *operations);
-void process_close_bracket(char *string, int *i, stack_c *operations, stack_c *out);
-void process_binary_operation(char *string, int *i, stack_c *operations, stack_c *out);
+void process_number(char *string, int *i, stack_t *out);
+void process_x(char *string, int *i, stack_t *out);
+void process_prefix_function(char *string, int *i, stack_t *operations);
+void process_close_bracket(char *string, int *i, stack_t *operations, stack_t *out);
+void process_binary_operation(char *string, int *i, stack_t *operations, stack_t *out);
 
 int is_prefix_function(char symbol);
 
 int get_priority(char symbol);
-void set_space(stack_c *out);
+void set_space(stack_t *out);
 
 #endif // SRC_MODELS_TRANSLATOR_H_
