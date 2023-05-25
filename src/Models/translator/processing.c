56da283e2d6b0339c0e3db56beb1ab5910b266fb
cheremshin <sergey.cheremshin@gmail.com>
2023-05-25 23:57:44 +0700

    feat: Change logic according to new stack functions

--- src/Models/translator/processing.c
+++ src/Models/translator/processing.c
@@ -1,45 +1,59 @@
 #include "translator.h"
 
 
-void process_number(char *string, int *i, stack_c *out) {
+void process_number(char *string, int *i, stack_t *out) {
     while (is_digit(string[*i]) || is_dot(string[*i])) {
-        push(out, string[(*i)++]);
+        push(out, &string[(*i)++]);
     }
 }
 
-void set_space(stack_c *out) {
-    if (is_digit(peek(out))) {
-        push(out, 0);
+void set_space(stack_t *out) {
+    char value;
+
+    peek(out, &value);
+    if (is_digit(value)) {
+        value = 0;
+        push(out, &value);
     }
 }
 
-void process_x(char *string, int *i, stack_c *out) {
-    push(out, string[(*i)++]);
+void process_x(char *string, int *i, stack_t *out) {
+    push(out, &string[(*i)++]);
 }
 
-void process_prefix_function(char *string, int *i, stack_c *operations) {
-    push(operations, string[(*i)++]);
+void process_prefix_function(char *string, int *i, stack_t *operations) {
+    push(operations, &string[(*i)++]);
 }
 
-void process_close_bracket(char *string, int *i, stack_c *operations, stack_c *out) {
-    while (peek(operations) != '(' && peek(operations) != 0) {
-        push(out, pop(operations));
+void process_close_bracket(char *string, int *i, stack_t *operations, stack_t *out) {
+    char value;
+    peek(operations, &value);
+    
+    while (!is_open_bracket(value) && operations->top != 0) {
+        pop(operations, &value);
+        push(out, &value);
+        peek(operations, &value);
     }
-    pop(operations);
+
+    pop(operations, &value); // Pop open bracket
     (*i)++;
 }
 
-void process_binary_operation(char *string, int *i, stack_c *operations, stack_c *out) {
-    if (peek(operations) != '(') {
-        char stack_top = peek(operations);
+void process_binary_operation(char *string, int *i, stack_t *operations, stack_t *out) {
+    char stack_top;
+
+    peek(operations, &stack_top);
+    if (!is_open_bracket(stack_top)) {
+        peek(operations, &stack_top);
         char op = string[*i];
 
         while ((is_prefix_function(stack_top)) ||
-              ((get_priority(stack_top) >= get_priority(op)) && stack_top != '(')) {
-            push(out, pop(operations));
-            stack_top = peek(operations);
+              ((get_priority(stack_top) >= get_priority(op)) && !is_open_bracket(stack_top))) {
+            pop(operations, &stack_top);
+            push(out, &stack_top);
+            peek(operations, &stack_top);
         }
     }
 
-    push(operations, string[(*i)++]);
+    push(operations, &string[(*i)++]);
 }
