2b3faf663766193755f84f18a09b479d8021398d
Skyte Kelsi <skytekel@in-e6.nsk.21-school.ru>
2023-05-26 21:28:58 +0700

    feat: Add test build

--- src/Models/test.c
+++ src/Models/test.c
@@ -7,13 +7,15 @@
 
 
 int main(void) {
-    //char *raw_expression = "123*15 - 27 ^ 2";
-    char *raw_expression = "123+5*-20^cos(30)";
+    //char *raw_expression = "123*15-10^cos(30)";
+    char *raw_expression = "123+5*20^cos(30)";
     printf("%s\n", raw_expression);
     char *prepared_expression = prepare_to_translate(raw_expression);
     printf("%s\n", prepared_expression);
     char *translated_expression = translate_to_polish(prepared_expression);
     printf("%s\n", translated_expression);
-    double result = calculate(translated_expression, NULL);
-    printf("%f", result);
+    double result = 0;
+    result = calculate(translated_expression, NULL);
+    printf("%f\n", result );
+    return 0;
 }
