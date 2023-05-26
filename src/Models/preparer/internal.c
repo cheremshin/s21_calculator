fc19d41065e385e8a919af5a71387c930d9c5bf3
Skyte Kelsi <skytekel@ob-g3.nsk.21-school.ru>
2023-05-26 14:02:28 +0700

    feat: Add support of unary minus and plus

--- src/Models/preparer/internal.c
+++ src/Models/preparer/internal.c
@@ -18,10 +18,11 @@ void convert_symbols(char *string) {
     for (int i = 0, j = 0; string[i] != '\0'; i++) {
         if (is_digit(string[i]) ||
             is_dot(string[i]) ||
-            is_operation(string[i]) ||
             is_x(string[i]) ||
             is_open_bracket(string[i]) ||
             is_close_bracket(string[i])) {
+            copy[j++] = string[i];
+        } else if (is_binary_operation(string[i] && is_digit(string[i - 1]))) {
             copy[j++] = string[i];
         } else {
             accept_shortcuts(string, copy, &i, &j);
@@ -36,6 +37,14 @@ void accept_shortcuts(char *string, char *copy, int *i, int *j) {
     int status = 1;
     char operation[5];
     char shortcut;
+
+    if (string[*i] == '-') {
+        status = 0;
+        copy[(*j)++] = '~';
+    } else if (string[*i] == '+') {
+        status = 0;
+        copy[(*j)++] = '$';
+    }
 
     for (int k = 0; k < SHORTCUTS_COUNT && status; k++) {
         strcpy(operation, operation_shortcuts[k].operation);
