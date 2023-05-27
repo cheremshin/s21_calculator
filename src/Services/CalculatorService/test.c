99bc1a81cb21710f081d2d7c44eb5ddf5aa78e10
cheremshin <sergey.cheremshin@gmail.com>
2023-05-28 05:06:24 +0700

    test: Change test makefile 'all' target

--- src/Services/CalculatorService/test.c
+++ src/Services/CalculatorService/test.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "preparer/preparer.h"
-#include "translator/translator.h"
-#include "calculator/calculator.h"
+#include "calculator_service.h"
 
 
 int main(void) {
     //char *raw_expression = "123*15-10^cos(30)";
-    char *raw_expression = "123+5*20^cos(30)";
-    printf("%s\n", raw_expression);
-    char *prepared_expression = prepare_to_translate(raw_expression);
-    printf("%s\n", prepared_expression);
-    char *translated_expression = translate_to_polish(prepared_expression);
-    printf("%s\n", translated_expression);
+    char *raw_expression = "11/6+56-12^(1/3)";
     double result = 0;
-    result = calculate(translated_expression, NULL);
-    printf("%f\n", result );
+    calculate_value(raw_expression, NULL, &result);
+    printf("%f", result);
     return 0;
 }
