a2df40b3d3941063c63d499c2855ca2f62f05794
cheremshin <sergey.cheremshin@gmail.com>
2023-06-07 05:21:20 +0700

    feat: Add support of stylesheet file, some configuration variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QtGui>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QQuickView>
 
 
@@ -7,8 +8,18 @@ int main(int argc, char *argv[]) {
     // App
     QGuiApplication app(argc, argv);
 
+
+    // Register types
+    qmlRegisterSingletonType(QUrl("qrc:/StyleSheet.qml"), "S21Calculator.style", 1, 0, "Style");
+
+
     // Startup
     QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("appDir", qApp->applicationDirPath());
+    engine.rootContext()->setContextProperty("config", qApp->applicationDirPath() + "/config");
+
+    engine.addImportPath("qrc:/");
 
     const QUrl url("qrc:/main.qml");
     engine.load(url);
