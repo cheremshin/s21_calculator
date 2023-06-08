5ff115a906eab0c035a16917179245481720741d
cheremshin <sergey.cheremshin@gmail.com>
2023-06-08 11:58:21 +0700

    feat: Add own fonts support

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include <QQuickView>
 
 
+void loadFonts();
+
 int main(int argc, char *argv[]) {
     // App
     QGuiApplication app(argc, argv);
 
+    loadFonts();
 
     // Register types
     qmlRegisterSingletonType(QUrl("qrc:/StyleSheet.qml"), "S21Calculator.style", 1, 0, "Style");
@@ -25,4 +28,24 @@ int main(int argc, char *argv[]) {
     engine.load(url);
 
     return app.exec();
+}
+
+void loadFonts() {
+    QString fontsPath = QGuiApplication::applicationDirPath() + "/resources/fonts/";
+    QDir fontsDir(fontsPath);
+    if (!fontsDir.isEmpty() && fontsDir.exists()) {
+        QDirIterator it(fontsPath, QDir::Files);
+
+        while (it.hasNext()) {
+            int res = QFontDatabase::addApplicationFont(it.next());
+            if (res == -1) {
+                qWarning() << QString("Loading font file: %1 failed.").arg(it.fileName());
+            }
+        }
+    } else {
+        qWarning() << QString("Unable to load application fonts from %1").arg(fontsPath);
+    }
+
+    QFont defaultFont("Inter");
+    QGuiApplication::setFont(defaultFont);
 }
